0cd7382ca2f01de71c8368321f179201060a2477
alex <alexandry.augustin@gmail.com>
2017-05-10 10:14:16 +0100

    Further work

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,78 +1,73 @@
 import sys
 from PyQt4 import QtGui, QtCore
+from ListWidget import ListWidget
 
-class ListWidget(QtGui.QListWidget):
+class MainWindow(QtGui.QMainWindow):
 	def __init__(self):
-		super(ListWidget, self).__init__()
-		self.initUI()
-
-	def initUI(self):               
-		self.setViewMode(QtGui.QListView.ListMode)
-		self.setIconSize(QtCore.QSize(400, 400))
-		self.setResizeMode(QtGui.QListWidget.Adjust);
-
-		item1 = QtGui.QListWidgetItem(QtGui.QIcon("./../res/pics/a.jpg"), "a.jpg")
-		item1.text() #hide
-		self.addItem(item1)
-
-		item2 = QtGui.QListWidgetItem(QtGui.QIcon("./../res/pics/b.jpg"), "b.jpg")
-		item2.icon() #hide
-		self.addItem(item2)
-
-		item3 = QtGui.QListWidgetItem(QtGui.QIcon("./../res/pics/c.jpg"), "c.jpg")
-		self.addItem(item3)
-
-	def setListMode(self):
-		self.setViewMode(QtGui.QListView.ListMode)
-
-	def setIconMode(self):
-		self.setViewMode(QtGui.QListView.IconMode)
-
-	def mousePressEvent(self, QMouseEvent):
-		print QMouseEvent.pos()
-		if QMouseEvent.button() == QtCore.Qt.LeftButton :
-			self.setListMode()
-		if QMouseEvent.button() == QtCore.Qt.RightButton :
-			self.setIconMode()
-
-	def mouseReleaseEvent(self, QMouseEvent):
-		cursor =QtGui.QCursor()
-#		print cursor.pos()        
-
-	def wheelEvent(self, event):
-		pass
-#		print("scrolled")
-
-class ui(QtGui.QWidget):
-	def __init__(self):
-		super(ui, self).__init__()
+		super(MainWindow, self).__init__()
 		self.initUI()
 
 	def initUI(self):               
 		self.setGeometry(0, 0, 1024, 768)
 		self.setWindowTitle('pwd')
-
+		#------------------------------------------
+		# ListWidget
+		#------------------------------------------
 		self.listWidget = ListWidget()
-		self.statusBar = QtGui.QStatusBar()
-		self.statusBar.showMessage("Ready")
+		#------------------------------------------
+		# Actions
+		#------------------------------------------
+		home = QtGui.QAction(QtGui.QIcon("../res/icones/basic_home.svg"), "home", self)
 
-		self.toolBar = QtGui.QToolBar("File")
-		
-		new = QtGui.QAction(QtGui.QIcon("new.bmp"),"new",self)
-		self.toolBar.addAction(new)
+		self.toggleView = QtGui.QAction(QtGui.QIcon("../res/icones/arrows_hamburger 2.svg"), "toggleView", self)
+		self.toggleView.triggered.connect(self.listWidget.toggleMode)
+
+		self.exitAction=QtGui.QAction(QtGui.QIcon('../res/icones/arrows_remove.svg'), '&Exit', self)
+		self.exitAction.setShortcut('Ctrl+Q')
+		self.exitAction.setStatusTip('Exit')
+		self.exitAction.triggered.connect(QtGui.qApp.quit)
+
+		self.refreshAction=QtGui.QAction(QtGui.QIcon('../res/icones/arrows_clockwise.svg'), '&Refresh', self)
+		self.refreshAction.setShortcut('Ctrl+R')
+		self.refreshAction.setStatusTip('Refresh')
+		#------------------------------------------
+		# Menu Bar
+		#------------------------------------------
+		self.fileMenu=self.menuBar().addMenu('&File')
+#		self.fileMenu.addAction(self.restartAction)
+		self.fileMenu.addAction(self.exitAction)
+
+		self.helpMenu=self.menuBar().addMenu('&Help')
+		self.helpMenu.addAction("About")
+		#------------------------------------------
+		# ToolBar
+		#------------------------------------------
+		self.toolBar = self.addToolBar("File")
+		self.toolBar.addAction(home)
+		self.toolBar.addAction(self.toggleView)
+		self.toolBar.addAction(self.refreshAction)
+		self.toolBar.addAction(self.exitAction)
 		
 #		self.toolBar.actionTriggered[QAction].connect(self.toolbtnpressed)
-
+		#------------------------------------------
+		# StatusBar
+		#------------------------------------------
+		self.statusBar().showMessage("Ready")
+		#------------------------------------------
+		# Central Widget
+		#------------------------------------------
 		layout = QtGui.QVBoxLayout()
 		layout.setMargin(0)
-		layout.addWidget(self.toolBar)
 		layout.addWidget(self.listWidget)
-		layout.addWidget(self.statusBar)
-		self.setLayout(layout)
+
+		self.centralWidget = QtGui.QWidget()
+		self.centralWidget.setLayout(layout)
+
+		self.setCentralWidget(self.centralWidget)
 
 		self.show()
 
 if __name__=="__main__":
 	app = QtGui.QApplication(sys.argv) 
-	ui = ui()
+	mainWindow = MainWindow()
 	sys.exit(app.exec_())
